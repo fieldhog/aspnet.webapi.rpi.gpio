3688616380aad7a5422f8229e92a74ceae38b8eb
Graham Fieldhouse <g_fieldhouse@hotmail.com>
2020-08-19 15:20:25 +0100

    Stablized flicking on display and added number output

--- aspnet.webapi.rpi.gpio/Program.cs
+++ aspnet.webapi.rpi.gpio/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading;
 using Unosquare.RaspberryIO;
@@ -53,18 +54,66 @@ namespace aspnet.webapi.rpi.gpio
             clockPin.PinMode = GpioPinDriveMode.Output;
 
             var pic = new []{ 0x1c, 0x22, 0x51, 0x45, 0x45, 0x51, 0x22, 0x1c };
+            var clr = new[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-            for (var count = 0; count < 50; count++) {
+            var nine = new[] { 0x00, 0x00, 0x32, 0x49, 0x49, 0x3E, 0x00, 0x00 };
+            var eight = new[] { 0x00, 0x00, 0x36, 0x49, 0x49, 0x36, 0x00, 0x00 };
+            var seven = new[] { 0x00, 0x00, 0x60, 0x47, 0x48, 0x70, 0x00, 0x00 };
+            var six = new[] { 0x00, 0x00, 0x3E, 0x49, 0x49, 0x26, 0x00, 0x00 };
+            var five = new[] { 0x00, 0x00, 0x79, 0x49, 0x49, 0x46, 0x00, 0x00 };
+            var four = new[] { 0x00, 0x00, 0x0E, 0x32, 0x7F, 0x02, 0x00, 0x00 };
+            var three = new[] { 0x00, 0x00, 0x22, 0x49, 0x49, 0x36, 0x00, 0x00 };
+            var two = new[] { 0x00, 0x00, 0x23, 0x45, 0x49, 0x31, 0x00, 0x00 };
+            var one = new[] { 0x00, 0x00, 0x21, 0x7F, 0x01, 0x00, 0x00, 0x00 };
+            var zero = new[] { 0x00, 0x00, 0x3E, 0x41, 0x41, 0x3E, 0x00, 0x00 };
+
+
+            MatrixOut(pic, TimeSpan.FromSeconds(5), latchPin, dataPin, clockPin);
+            MatrixOut(clr, TimeSpan.FromMilliseconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(nine, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(eight, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(seven, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(six, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(five, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(four, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(three, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(two, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(one, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(zero, TimeSpan.FromSeconds(1), latchPin, dataPin, clockPin);
+            MatrixOut(clr, TimeSpan.FromMilliseconds(1), latchPin, dataPin, clockPin);
+        }
+
+        static void MatrixOut(int[] matrix, TimeSpan timeSpan, IGpioPin latchPin, IGpioPin dataPin, IGpioPin clockPin)
+        {
+            var startTime = DateTime.UtcNow;
+
+            while (DateTime.UtcNow - startTime < timeSpan)
+            {
                 var column = 0x80;
                 for (var i = 0; i < 8; i++)
                 {
                     latchPin.Write(false);
-                    ShiftOut(dataPin, clockPin, false, pic[i]);// first shift data of line information to the first stage 74HC959
-                    ShiftOut(dataPin, clockPin, true, ~column);//then shift data of column information to the second stage 74HC959
+                    ShiftOut(dataPin, clockPin, false, matrix[i]);// first shift data of line information to the first stage 74HC959
+                    ShiftOut(dataPin, clockPin, false, ~column);//then shift data of column information to the second stage 74HC959
                     latchPin.Write(true); //Output data of two stage 74HC595 at the same 
-
                     column >>= 1;   //display the next columndelay(1);
+                    Thread.Sleep(2); //Try and keep the column LEDs on a short time to make them brighter.
                 }
+            }
+        }
+
+        static void ShiftOut(IGpioPin dPin, IGpioPin cPin, bool lsbFirst, int val)
+        {
+            int i; for (i = 0; i < 8; i++)
+            {
+                cPin.Write(false);
+
+                if (lsbFirst)
+                    dPin.Write((0x01 & (val >> i)) == 0x01);
+                else
+                    dPin.Write((0x80 & (val << i)) == 0x80);
+
+                cPin.Write(true);
             }
         }
 
@@ -86,24 +135,6 @@ namespace aspnet.webapi.rpi.gpio
                 ShiftOut(dataPin, clockPin, true, c);
                 latchPin.Write(true);
                 Thread.Sleep(updateDelay);
-            }
-        }
-
-        static void ShiftOut(IGpioPin dPin, IGpioPin cPin, bool lsbFirst, int val)
-        {
-            const int shiftDelay = 1;
-
-            int i; for (i = 0; i < 8; i++)
-            {
-                cPin.Write(false);
-
-                if (lsbFirst)
-                    dPin.Write((0x01 & (val >> i)) == 0x01); 
-                else
-                    dPin.Write((0x80 & (val << i)) == 0x80);
-
-                Thread.Sleep(shiftDelay);
-                cPin.Write(true);
             }
         }
 
